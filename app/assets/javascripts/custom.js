2cf3f77e99512ff764322f989ab575b8bcb5e635
Ubuntu <ubuntu@ip-172-31-22-170.ap-northeast-1.compute.internal>
2022-01-03 05:47:13 +0000

    Finish preview_image

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -12,3 +12,31 @@ $(document).on('turbolinks:load', function() {
     });
   });
 });
+
+//捜索願のプレビュー
+$(function(){
+    $("#preview").on("change", function(){
+      let selectedFile = this.files[0];
+      let fileReader = new FileReader();
+      
+      fileReader.onload = function(e){
+        $(".preview_wanted").attr("src", e.target.result);
+      };
+      
+      fileReader.readAsDataURL(selectedFile);
+    });
+});
+
+//アイコン画像のプレビュー
+$(function(){
+    $("#preview_icon").on("change", function(){
+      let selectedIcon = this.files[0];
+      let fileReader = new FileReader();
+      
+      fileReader.onload = function(e){
+        $(".user-icon").attr("src", e.target.result);
+      };
+      
+      fileReader.readAsDataURL(selectedIcon);
+    });
+});
